282be3889d6674be194bd56a3fdff4d906f21c7e
Claus Jensby Madsen <clausjensbymadsen@gmail.com>
2021-06-29 15:59:52 +0200

    Randon number generator

---
+++ src/random.c
@@ -0,0 +1,21 @@
+#ifndef RANDOM_C
+#define RANDOM_C
+
+#include "random_raw.c"
+
+static inline char random_char(size_t time)
+{
+  return random_data[time % RANDOM_DATA_SIZE];
+}
+
+static inline int8_t random_int8(size_t time)
+{
+  return (int8_t)random_char(time);
+}
+
+static inline uint8_t random_uint8(size_t time)
+{
+  return (uint8_t)random_char(time);
+}
+
+#endif
