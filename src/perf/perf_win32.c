27c40c94331f0a9b2803fe6bf32d813f9bb8fd05
Claus Jensby Madsen <clausjensbymadsen@gmail.com>
2021-06-29 21:44:27 +0200

    Implement performance measurer

--- src/perf/perf_win32.c
+++ src/perf/perf_win32.c
@@ -3,35 +3,108 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-LARGE_INTEGER freq, start, stop;
+#include "../sensorsim.c"
+
+LARGE_INTEGER qp_freq, qp_start;
 
 static inline void stopwatch_init()
 {
-  QueryPerformanceFrequency(&freq);
+  QueryPerformanceFrequency(&qp_freq);
 }
 
 static inline void stopwatch_start()
 {
-  QueryPerformanceCounter(&start);
+  QueryPerformanceCounter(&qp_start);
 }
 
-static inline void stopwatch_stop()
+static inline void stopwatch(LARGE_INTEGER* li)
 {
-  QueryPerformanceCounter(&stop);
+  QueryPerformanceCounter(li);
 }
 
-static inline size_t stopwatch_us()
+static inline size_t stopwatch_us(LARGE_INTEGER* stop)
 {
-  return (stop.QuadPart - start.QuadPart) * 1000000 / freq.QuadPart;
+  return (stop->QuadPart - qp_start.QuadPart) * 1000000 / qp_freq.QuadPart;
+}
+
+#define RIGHT_JUSTIFICATION 40
+
+typedef struct {
+  unsigned short data;
+  LARGE_INTEGER timestamp;
+} sample_t;
+
+#define SAMPLE_SIZE 25000
+sample_t samples[SAMPLE_SIZE];
+
+static void indent(unsigned int num)
+{
+  for(unsigned int i = 0; i < num; ++i)
+  {
+    printf(" ");
+  }
+}
+
+static void print_sample_result(int x)
+{
+  for(int i = x; i < RIGHT_JUSTIFICATION; ++i) printf(" ");
+
+  size_t min = UINT64_MAX;
+  size_t max = 0;
+  size_t sum = 0;
+  for(int i = 0; i < SAMPLE_SIZE; ++i)
+  {
+    size_t time = stopwatch_us(&samples[i].timestamp);
+    if (time < min) min = time;
+    if (time > max) max = time;
+    sum += time;
+  }
+  printf("%uus / %uus / %uus\n", min, sum/25000, max);
 }
 
 int main()
 {
+  printf("Sample size: %u\n\n", SAMPLE_SIZE);
   stopwatch_init();
 
+  printf("white_noise:");
+  for(size_t i = 0; i < SAMPLE_SIZE; ++i)
+  {
+    samples[i].data = white_noise(i);
+  }
   stopwatch_start();
-  Sleep(100);
-  stopwatch_stop();
+  for(size_t i = 0; i < SAMPLE_SIZE; ++i)
+  {
+    samples[i].data = white_noise(i);
+    stopwatch(&(samples[i].timestamp));
+  }
+  print_sample_result(12);
+
+  printf("harmonics_peak:");
+  for(size_t i = 0; i < SAMPLE_SIZE; ++i)
+  {
+    samples[i].data = harmonics_peak(i);
+  }
+  stopwatch_start();
+  for(size_t i = 0; i < SAMPLE_SIZE; ++i)
+  {
+    samples[i].data = harmonics_peak(i);
+    stopwatch(&(samples[i].timestamp));
+  }
+  print_sample_result(15);
+
+  printf("\ngetreading:");
+  for(size_t i = 0; i < SAMPLE_SIZE; ++i)
+  {
+    samples[i].data = getreading(i);
+  }
+  stopwatch_start();
+  for(size_t i = 0; i < SAMPLE_SIZE; ++i)
+  {
+    samples[i].data = getreading(i);
+    stopwatch(&(samples[i].timestamp));
+  }
+  print_sample_result(11);
 
   return 0;
 }
