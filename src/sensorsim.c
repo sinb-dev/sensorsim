c04a6dc990b29b29773c737d59665875098a6809
Claus Jensby Madsen <clausjensbymadsen@gmail.com>
2021-06-30 11:49:44 +0200

    Rename microsecond parameter

--- src/sensorsim.c
+++ src/sensorsim.c
@@ -29,15 +29,15 @@
 
 //-------------------------------------------------------------------------//
 
-unsigned short white_noise(size_t microsecond)
+unsigned short white_noise(size_t time)
 {
-  return random_ushort(microsecond) % WHITE_NOISE_AMPLITUDE;
+  return random_ushort(time) % WHITE_NOISE_AMPLITUDE;
 }
 
-unsigned short harmonics_peak(size_t microsecond)
+unsigned short harmonics_peak(size_t time)
 {
-    double parable_x = round(microsecond / (double)HARMONIC_PEAK_DISTANCE) * HARMONIC_PEAK_DISTANCE;
-    double x = microsecond - parable_x;
+    double parable_x = round(time / (double)HARMONIC_PEAK_DISTANCE) * HARMONIC_PEAK_DISTANCE;
+    double x = time - parable_x;
     double a = -HARMONIC_PEAK_WIDTH;
     double c = HARMONIC_PEAK_AMPLITUDE;
 
@@ -45,27 +45,27 @@ unsigned short harmonics_peak(size_t microsecond)
     return y > 0 ? y : 0;
 }
 
-unsigned short getreading(size_t microsecond)
+unsigned short getreading(size_t time)
 {
   // Add white noise to the reading as baseline
-  unsigned short value = white_noise(microsecond);
+  unsigned short value = white_noise(time);
 
   //Add harmonic peaks. These are expected for proper functioning devices.
-  value += harmonics_peak(microsecond);
+  value += harmonics_peak(time);
 
   // The time the device starts malfunctioning.
   size_t malfunction_threshold = MALFUNCTION_THRESHOLD_MINIMUM + random_uint8(0) * MALFUNCTION_THRESHOLD_VARIATION;
 
-  if (microsecond > malfunction_threshold)
+  if (time > malfunction_threshold)
   {
     // Device is old. Introduce pathological behaviour into the simulation.
-    size_t malfunction_age = microsecond - malfunction_threshold;
+    size_t malfunction_age = time - malfunction_threshold;
 
     // The more time has passed since the malfunction threshold, the greater
     // the propability of erratic behaviour in the readings.
-    if (random_uint16(microsecond) < malfunction_age) 
+    if (random_uint16(time) < malfunction_age) 
     {
-      switch(random_uint8(microsecond) & 0x04)
+      switch(random_uint8(time) & 0x04)
       {
         case 0:
           __attribute__((fallthrough));
