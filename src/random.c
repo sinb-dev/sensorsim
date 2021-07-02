2a635d4cb03aeaca817ef035b6e98edb721391e3
Claus Jensby Madsen <clausjensbymadsen@gmail.com>
2021-07-02 16:59:44 +0200

    Fix random bug

--- src/random.c
+++ src/random.c
@@ -19,17 +19,17 @@ static inline uint8_t random_uint8(size_t time)
 
 static inline uint16_t random_uint16(size_t time)
 {
-  return ((uint16_t)random_uint8(time) << 8) | random_uint8(time);
+  return ((uint16_t)random_uint8(time) << 8) | random_uint8(time+1);
 }
 
 static inline uint32_t random_uint32(size_t time)
 {
-  return ((uint32_t)random_uint16(time) << 16) | random_uint16(time);
+  return ((uint32_t)random_uint16(time) << 16) | random_uint16(time+2);
 }
 
 static inline uint64_t random_uint64(size_t time)
 {
-  return ((uint64_t)random_uint32(time) << 32) | random_uint32(time);
+  return ((uint64_t)random_uint32(time) << 32) | random_uint32(time+4);
 }
 
 static inline unsigned short random_ushort(size_t time)
@@ -37,6 +37,7 @@ static inline unsigned short random_ushort(size_t time)
   if (sizeof(unsigned short) == 1) return random_uint8(time);
   if (sizeof(unsigned short) == 2) return random_uint16(time);
   if (sizeof(unsigned short) == 4) return random_uint32(time);
+  if (sizeof(unsigned short) == 8) return random_uint64(time);
   assert(0);
 }
 
